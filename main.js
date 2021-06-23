1c16fa86e1c1023b14adc7c4af3a5e3ca8a10a40
kayleekim409 <81453056+kayleekim409@users.noreply.github.com>
2021-06-23 10:53:14 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+car1_width = 120;
+car1_height = 70;
+car1_image = "car1.png";
+car1_x = 10;
+car1_y = 10;
+
+car2_width = 120;
+car2_height = 70;
+car2_image = "car1.png";
+car2_x = 10;
+car2_y = 10;
+background_image = "racing.jpg";
+function add(){
+    background_imgTag = new Image();
+    background_imgTag.onload = uploadBackground;
+    background_imgTag.src = background_image;
+
+    car1_imgTag = new Image();
+    car1_imgTag.onload = uploadcar1;
+    car1_imgTag.src = car1_image;
+}
